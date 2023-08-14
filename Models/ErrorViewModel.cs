016c270dff0c4ed8aecace8c1a62acedea9e7744
Muhammad Sabry <mohammed20191700542@cis.asu.edu.eg>
2023-08-14 18:18:51 +0300

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace African_Football_Legends.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
